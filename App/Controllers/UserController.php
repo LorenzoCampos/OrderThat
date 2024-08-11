74bfe6ca6fb1084edf9ce6138d4bb66661c26496
CannataJulian <cannatajulian@gmail.com>
2024-08-11 20:17:05 -0300

    modificación controller myAccount, integrado validación controller myAccount, integrado controller changePassword

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -13,38 +13,6 @@ class UserController extends Controller
         return $this->view('login');
     }
 
-    // public function loginRequest()
-    // {
-    //     $user = new User();
-
-    //     $request = $_POST;
-
-    //     $email = $request['email'];
-
-    //     $password = $request['password'];
-
-    //     $validation_email = $user->where('email', "$email")->first();
-
-    //     $user_validation = new User();
-
-    //     $validation = $user_validation->where('email', "$email")->where('password', "$password")->first();
-
-    //     if ($validation)
-    //     {
-    //         session_start();
-
-    //         $_SESSION['id_user'] = $validation['id'];
-
-    //         header("Location: ../public/");
-    //     }
-    //     else
-    //     {
-    //         $_SESSION['error_message'] = "Error al iniciar sesion";
-
-    //         header("Location: ../public/login");
-    //     }
-    // }
-
     public function loginRequest()
     {
         $user = new User();
@@ -91,6 +59,8 @@ class UserController extends Controller
         }
 
         header("Location: ../public/login");
+
+        exit;
     }
 
     public function logout()
@@ -159,9 +129,9 @@ class UserController extends Controller
 
         $id_user = $_SESSION['id_user'];
 
-        $before_request = $user->where('id', "$id_user")->first();
+        $request = $user->where('id', "$id_user")->first();
 
-        return $this->view('myAccount', compact('before_request'));
+        return $this->view('myAccount', compact('request'));
     }
 
     public function myAccountRequest()
@@ -170,53 +140,90 @@ class UserController extends Controller
         
         $id_user = $_SESSION['id_user'];
 
-        if (isset($_POST['current_password']))
+        $request = $_POST;
+
+        $current_user = $user->where('id', "$id_user")->first();
+
+        if ($current_user['email'] != $request['email'])
         {
-            $update_request_user = $_POST;
-            $email = $_POST['email'];
+            $user_test = new User();
 
-            echo var_dump($update_request_user);
+            $email = $request['email'];
 
-            $current_user = $user->where('id', "$id_user")->first();
+            $validation_email = $user_test->where('email', "$email")->first();
 
-            $cant_users = $user->where('email', "lolo@lolo.com")->get();
-
-            echo var_dump($cant_users);
-
-            if ($update_request_user['current_password'] == $current_user['password'])
+            if ($validation_email)
             {
-                if ($update_request_user['new_password'] == $update_request_user['repeat_new_password'])
-                {
-                    $update_user['first_name'] = $update_request_user['first_name'];
-                    $update_user['last_name'] = $update_request_user['last_name'];
-                    $update_user['email'] = $update_request_user['email'];
-                    $update_user['password'] = $update_request_user['new_password'];
+                $_SESSION['error_message'] = "El email ya existe";
 
-                    $validation = $user->update("$id_user", $update_user);
+                header("Location: ../public/myAccount");
 
-                    if ($validation)
-                    {
-                        $id_user = $_SESSION['id_user'];
-
-                        // header('Location: ../public/myAccount');
-                    } else
-                    {
-                        $_SESSION['error_message'] = "Error al subir a la base de datos";
-                    }
-                } else {
-                    $_SESSION['error_message'] = "Las contrasenias no coinciden";
-                }
-            } else {
-                $_SESSION['error_message'] = "La nueva contrasenia no coincide con la actual";
+                exit;
             }
+        }
 
-            // header('Location: ../public/myAccount');
-
-        } else
+        if ($current_user['email'] == $request['email'])
         {
-            $request = $user->where('id', "$id_user")->first();
+            unset($request['email']);
+        }
 
-            return $this->view('myAccount', compact('request'));
+        $validation = $user->update("$id_user", $request);
+
+        if ($validation)
+        {
+            $_SESSION['error_message'] = "Cuenta actualizada";
+
+            header("Location: ../public/myAccount");
+
+            exit;
+        }
+        else
+        {
+            $_SESSION['error_message'] = "Error al actualizar cuenta";
+
+            header("Location: ../public/myAccount");
+
+            exit;
+        }
+    }
+
+    public function changePassword()
+    {
+        $requestPassword = "Texto";
+
+        return $this->view('myAccount', compact('requestPassword'));
+    }
+    public function changePasswordRequest()
+    {
+        $user = new User();
+
+        $id_user = $_SESSION['id_user'];
+
+        $update_password = $_POST;
+
+        $current_password = $user->where('id', "$id_user")->first();
+
+        if ($current_password['password'] == $update_password['current_password'])
+        {
+            if ($current_password['password'] == $update_password['new_password'])
+            {
+                if ($update_password['new_password'] == $update_password['confirm_new_password'])
+                {
+                    $validation = $user->update("$id_user", $update_password);
+                }
+                else
+                {
+                    $_SESSION['error_message'] = "La nueva contraseña y la confirmación no coinciden";
+                }
+            }
+            else
+            {
+                $_SESSION['error_message'] = "La nueva contraseña y na actual no pueden ser iguales";
+            }
+        }
+        else
+        {
+            $_SESSION['error_message'] = "Tu contrseña actual no coincide";
         }
     }
 
