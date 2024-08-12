9c191a448132f56a2db7b113dc011f2805752ce1
CannataJulian <cannatajulian@gmail.com>
2024-08-12 17:32:57 -0300

    Alertas para datos personales y contraseñas

---
+++ resources/static/js/alerts.js
@@ -0,0 +1,11 @@
+// Alertas de confirmación
+
+// Modificar datos personales
+document.getElementById("buttonModif").addEventListener("click", function() {
+  alert("Se han modificado los datos personales!");
+});
+
+// Modificar contraseña
+document.getElementById("buttonModif2").addEventListener("click", function() {
+  alert("Se ha modificado la contraseña!");
+});
