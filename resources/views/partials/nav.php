4106f61b6a419be79b22497f48e8797a2b6b5961
LorenzoCampos <lolocampos544@gmail.com>
2024-08-17 02:00:39 -0300

    Agregado modularizacion de las vistas

--- resources/views/partials/nav.php
+++ resources/views/partials/nav.php
@@ -1,11 +1,34 @@
-<header>
-    <nav>
-        <div id="header-container">
-            <a href="../public"><img src="" alt="logo"></a>
-            <form action="" id="input-header">
-                <input class="search-bar" type="text" name="" id="" placeholder="Busqueda">
-            </form>
-            <a href="../public/login">Iniciar Sesión</a>
-        </div>
-    </nav>
-</header>
+<nav>
+	<div class="nav-container">
+		<div class="nav-item">
+			<a href="../public"><img src="../resources/static/img/Logo/rub-white.png"></a>
+			<input type="checkbox" class="theme-checkbox" id="darkMode">
+		</div>
+
+		<?php
+		if (isset($_SESSION['id_user'])) {
+		?>
+
+			<div class="nav-item-user">
+				<div class="nav-item">
+					<a href="../public/cart"><b>Carrito</b></a>
+					<a href="../public/myAccount"><b>Mi Cuenta</b></a>
+				</div>
+
+				<div class="nav-item">
+					<a href="../public/logout"><b>Cerrar Sesión</b></a>
+				</div>
+			</div>
+
+		<?php
+		} else {
+		?>
+			<div class="nav-item">
+				<a href="../public/login"><b>Iniciar Sesión</b></a>
+			</div>
+			
+		<?php
+		}
+		?>
+	</div>
+</nav>
---
+++ resources/views/partials/subNav.php
@@ -0,0 +1,10 @@
+<div class="sub-nav">
+	<div class="sub-nav__container">
+		<div class="sub-nav__container__items">
+			<a href="../public/myAccount"><p>Datos Personales</p></a>
+			<a href="../public/myAddress"><p>Direcciones</p></a>
+			<a href="../public/myPayment"><p>Metodos de Pago</p></a>
+			<a href="../public/myRecentOrders"><p>Historial de pedidos</p></a>
+		</div>
+	</div>
+</div>
