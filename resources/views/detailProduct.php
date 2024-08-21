99ab3dc53e8a3816f88606cfbead7264f52bc220
CannataJulian <cannatajulian@gmail.com>
2024-08-21 13:54:18 -0300

    Nuevo detail product

---
+++ resources/views/detailProduct.php
@@ -0,0 +1,31 @@
+<?php
+
+include "partials/initSession.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Inicio</title>
+	<link rel="icon" href="../resources/static/img/Logo/rub-white.png">
+	<link rel="stylesheet" href="../resources/static/css/main.css">
+	<link rel="stylesheet" href="../resources/static/css/home.css">
+</head>
+
+<?php
+
+include "partials/nav.php";
+
+?>
+
+<body>
+
+
+
+</body>
+
+</html>
