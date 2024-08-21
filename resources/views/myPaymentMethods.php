1cf7923ab52ada09adb1afad0f98b933850c4394
CannataJulian <cannatajulian@gmail.com>
2024-08-21 13:54:57 -0300

    Nuevo metodo de pago

---
+++ resources/views/myPaymentMethods.php
@@ -0,0 +1,30 @@
+<?php
+
+include "partials/initSession.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mis Metodos de Pago</title>
+  <link rel="icon" href="../resources/static/img/Logo/rub-white.png">
+  <link rel="stylesheet" href="../resources/static/css/main.css">
+  <link rel="stylesheet" href="../resources/static/css/address.css">
+  <link rel="stylesheet" href="../resources/static/css/dropdownMenu.css">
+</head>
+
+<?php
+
+include "partials/nav.php";
+
+include "partials/subNav.php";
+
+?>
+
+<body>
+  <h1>METODO DE PAGO</h1>
+</body>
+</html>
