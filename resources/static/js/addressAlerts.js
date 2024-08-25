3c5db0b87b539be34d13e5f444d4d6e5c01031fa
CannataJulian <cannatajulian@gmail.com>
2024-08-25 20:23:50 -0300

    alerta myAddress

--- resources/static/js/addressAlerts.js
+++ resources/static/js/addressAlerts.js
@@ -1,8 +1,14 @@
 /* Alertas de myAddress */
-  function deleteAddress(id) {
+function deleteAddress(id) {
 
-    if (confirm("Estas seguro?")){
-      window.location.href = "../public/deleteAddress/" + id;
-      alert("Se ha borrado su dirección!");
-    }
+  if (confirm("Estas seguro?")) {
+    window.location.href = "../public/deleteAddress/" + id;
+    alert("Se ha borrado su dirección!");
   }
+}
+
+document.getElementById("buttonModifAddress").addEventListener("click", function() {
+  alert("Se ha modificado su dirección!");
+
+  window.location.href = "../public/myAddress";
+});
