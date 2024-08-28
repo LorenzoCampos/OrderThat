374424b268c0e6244393477316177b60f5fe0c96
CannataJulian <cannatajulian@gmail.com>
2024-08-28 16:55:09 -0300

    menu nuevo

--- resources/views/partials/nav.php
+++ resources/views/partials/nav.php
@@ -1,33 +1,58 @@
 <nav>
-	<div class="nav-container">
-		<div class="nav-item">
-			<a href="../public"><img src="../resources/static/img/Logo/rub-white.png"></a>
-		</div>
-
 		<?php
 		if (isset($_SESSION['id_user'])) {
 		?>
 
-			<div class="nav-item-user">
-				<div class="nav-item">
-					<a href="../public/cart"><b>Carrito</b></a>
-					<a href="../public/myAccount"><b>Mi Cuenta</b></a>
-				</div>
+    <div class="nav-container">
 
-				<div class="nav-item">
-					<a href="../public/logout"><b>Cerrar Sesión</b></a>
-				</div>
-			</div>
+      <div class="nav-item"></div>
 
-		<?php
+        <div class="user-container">
+          
+          <div class="user-container__button">
+            <img src="../resources/static/img/default/menu.png">
+
+            <div class="button-dropdown">
+              <a href="../public/"><b>Inicio</b></a>
+              <a href="../public/myAccount"><b>Cuenta</b></a>
+              <a href="../public/myAddress"><b>Direcciones</b></a>
+              <a href="../public/myPaymentMethods"><b>Metodos de pago</b></a>
+              <a href="../public/myRecentOrders"><b>Historial de pedidos</b></a>
+              <p>Sesión</p>
+              <a href="../public/logout"><b>Cerrar Sesión</b></a>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="nav-item">
+          <img class="nav-item__img" src="../resources/static/img/default/carrito.png" href="../">
+        </div>
+
+      </div>
+
+    </div>
+
+    <?php
 		} else {
 		?>
-			<div class="nav-item">
-				<a href="../public/login"><b>Iniciar Sesión</b></a>
-			</div>
-			
+    <div class="nav-container">
+      <div class="nav-item ">
+        <div class="user-container">
+          
+          <div class="user-container__button">
+            <img src="../resources/static/img/default/menu.png">
+
+            <div class="button-dropdown">
+              <a href="../public/login"><b>Iniciar Sesión</b></a>
+            </div>  
+      </div>
+    </div>
+
 		<?php
 		}
 		?>
-	</div>
+	  </div>
 </nav>
+
+<script src="../resources/static/js/nav.js"></script>
