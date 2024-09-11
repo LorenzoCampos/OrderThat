32e177dc5d862e8979f1ed64c88aa3d81126ff21
CannataJulian <cannatajulian@gmail.com>
2024-09-11 14:25:04 -0300

    BBDD sincronizada

--- config/orderthat.sql
+++ config/orderthat.sql
@@ -2,6 +2,91 @@ DROP IS EXISTS orderthat;
 CREATE DATABASE orderthat;
 USE orderthat;
 
+/* -- Tabla de usuarios
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20),
+    type VARCHAR(50)
+);
+
+-- Tabla de direcciones
+CREATE TABLE addresses (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_user INT,
+    locality VARCHAR(255),
+    street VARCHAR(255),
+    number INT,
+    floor INT,
+    street1 VARCHAR(255),
+    street2 VARCHAR(255),
+    indications TEXT,
+    FOREIGN KEY (fk_user) REFERENCES users(id)
+);
+
+-- Tabla de métodos de pago
+CREATE TABLE pay_methods (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_user INT,
+    description VARCHAR(255),
+    type VARCHAR(50),
+    FOREIGN KEY (fk_user) REFERENCES users(id)
+);
+
+-- Tabla de pedidos
+CREATE TABLE orders (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_user INT,
+    fk_payMethod INT,
+    state_order VARCHAR(50),
+    date DATE,
+    FOREIGN KEY (fk_user) REFERENCES users(id),
+    FOREIGN KEY (fk_payMethod) REFERENCES pay_methods(id)
+);
+
+-- Tabla de productos
+CREATE TABLE products (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    image_path VARCHAR(1000),
+    price FLOAT,
+    stock INT
+);
+
+-- Tabla de detalles de pedidos
+CREATE TABLE orders_details (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_product INT,
+    fk_order INT,
+    amount INT,
+    FOREIGN KEY (fk_product) REFERENCES products(id),
+    FOREIGN KEY (fk_order) REFERENCES orders(id)
+);
+
+-- Tabla de ingredientes
+CREATE TABLE ingredients (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_product INT,
+    ingredient VARCHAR(255),
+    FOREIGN KEY (fk_product) REFERENCES products(id)
+);
+
+-- Tabla del carrito
+CREATE TABLE carts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_user INT,
+    fk_product INT,
+    amount INT,
+    FOREIGN KEY (fk_product) REFERENCES products(id),
+    FOREIGN KEY (fk_user) REFERENCES users(id)
+); */
+
+-----------------------------------------------------------
+
 --
 -- Base de datos: `orderthat`
 --
@@ -309,3 +394,7 @@ ALTER TABLE `orders_details`
 ALTER TABLE `pay_methods`
   ADD CONSTRAINT `pay_methods_ibfk_1` FOREIGN KEY (`fk_user`) REFERENCES `users` (`id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
