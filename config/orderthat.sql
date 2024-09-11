6e16feb50d44b0b28b7da09d2b39afd051114357
CannataJulian <cannatajulian@gmail.com>
2024-09-11 14:19:14 -0300

    BBDD sincronizada

--- config/orderthat.sql
+++ config/orderthat.sql
@@ -2,85 +2,310 @@ DROP IS EXISTS orderthat;
 CREATE DATABASE orderthat;
 USE orderthat;
 
--- Tabla de usuarios
-CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(20),
-    type VARCHAR(50)
-);
+--
+-- Base de datos: `orderthat`
+--
 
--- Tabla de direcciones
-CREATE TABLE addresses (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    fk_user INT,
-    locality VARCHAR(255),
-    street VARCHAR(255),
-    number INT,
-    floor INT,
-    street1 VARCHAR(255),
-    street2 VARCHAR(255),
-    indications TEXT,
-    FOREIGN KEY (fk_user) REFERENCES users(id)
-);
+-- --------------------------------------------------------
 
--- Tabla de métodos de pago
-CREATE TABLE pay_methods (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    fk_user INT,
-    description VARCHAR(255),
-    type VARCHAR(50),
-    FOREIGN KEY (fk_user) REFERENCES users(id)
-);
+--
+-- Estructura de tabla para la tabla `addresses`
+--
 
--- Tabla de pedidos
-CREATE TABLE orders (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    fk_user INT,
-    fk_payMethod INT,
-    state_order VARCHAR(50),
-    date DATE,
-    FOREIGN KEY (fk_user) REFERENCES users(id),
-    FOREIGN KEY (fk_payMethod) REFERENCES pay_methods(id)
-);
+CREATE TABLE `addresses` (
+  `id` int(11) NOT NULL,
+  `fk_user` int(11) DEFAULT NULL,
+  `locality` varchar(255) DEFAULT NULL,
+  `street` varchar(255) DEFAULT NULL,
+  `number` int(11) DEFAULT NULL,
+  `floor` int(11) DEFAULT NULL,
+  `street1` varchar(255) DEFAULT NULL,
+  `street2` varchar(255) DEFAULT NULL,
+  `indications` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Tabla de productos
-CREATE TABLE products (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255),
-    description VARCHAR(255),
-    image_path VARCHAR(1000),
-    price FLOAT,
-    stock INT
-);
+-- --------------------------------------------------------
 
--- Tabla de detalles de pedidos
-CREATE TABLE orders_details (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    fk_product INT,
-    fk_order INT,
-    amount INT,
-    FOREIGN KEY (fk_product) REFERENCES products(id),
-    FOREIGN KEY (fk_order) REFERENCES orders(id)
-);
+--
+-- Estructura de tabla para la tabla `carts`
+--
 
--- Tabla de ingredientes
-CREATE TABLE ingredients (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    fk_product INT,
-    ingredient VARCHAR(255),
-    FOREIGN KEY (fk_product) REFERENCES products(id)
-);
+CREATE TABLE `carts` (
+  `id` int(11) NOT NULL,
+  `fk_user` int(11) DEFAULT NULL,
+  `fk_product` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Tabla del carrito
-CREATE TABLE carts (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    fk_user INT,
-    fk_product INT,
-    amount INT,
-    FOREIGN KEY (fk_product) REFERENCES products(id),
-    FOREIGN KEY (fk_user) REFERENCES users(id)
-);
+--
+-- Volcado de datos para la tabla `carts`
+--
+
+INSERT INTO `carts` (`id`, `fk_user`, `fk_product`, `amount`) VALUES
+(9, 1, 7, 3),
+(10, 1, 8, 3),
+(11, 3, 11, 1),
+(12, 3, 10, 1),
+(13, 3, 6, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ingredients`
+--
+
+CREATE TABLE `ingredients` (
+  `id` int(11) NOT NULL,
+  `fk_product` int(11) DEFAULT NULL,
+  `ingredient` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `fk_user` int(11) DEFAULT NULL,
+  `fk_payMethod` int(11) DEFAULT NULL,
+  `state_order` varchar(50) DEFAULT NULL,
+  `date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `orders_details`
+--
+
+CREATE TABLE `orders_details` (
+  `id` int(11) NOT NULL,
+  `fk_product` int(11) DEFAULT NULL,
+  `fk_order` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pay_methods`
+--
+
+CREATE TABLE `pay_methods` (
+  `id` int(11) NOT NULL,
+  `fk_user` int(11) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `image_path` varchar(1000) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `image_path`, `price`, `stock`) VALUES
+(6, 'Hamburguesa Común', 'Pan, medallón de carne, kétchup, mostaza y cebolla triturada.', '../resources/static/img/hamburguesa.png', 5000, 10),
+(7, 'Hamburguesa con queso', 'Carne, queso derretido y mostaza, kétchup y cebolla triturada.', '../resources/static/img/hamburguesa_con_queso.png', 6000, 10),
+(8, 'Doble Carne Doble Queso', 'Dos medallones de carne, queso cheddar.', '../resources/static/img/doble_carne_doble_queso.png', 7000, 10),
+(9, 'Triple Hamburguesa Con Queso', 'Tres medallones de carne 100% vacuna, queso derretido, mostaza, kétchup y cebolla triturada, es algo que nunca puede fallar.', '../resources/static/img/triple_hamburguesa_con_queso.png', 8000, 10),
+(10, 'Big Mac', 'Dos medallones de carne, salsa especial, queso, cebolla, lechuga y pepino.', '../resources/static/img/big_mac.png', 9000, 10),
+(11, 'Cuarto De Libra Con Queso', 'Dos medallones de carne, queso, mostaza, ketchup y cebolla.', '../resources/static/img/cuarto_de_libra_con_queso.png', 10000, 100),
+(12, 'Bcon Cheddar', 'Medallon de carne, bacon, cheddar y cebolla', '../resources/static/img/bacon_cheddar.png', 12000, 15);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `phone_number`, `type`) VALUES
+(1, '', '', 'aaaa', 'aaaa', NULL, NULL),
+(2, '', '', 'admin', 'admin', NULL, 'admin'),
+(3, '', '', 'user', 'user', '', NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `addresses`
+--
+ALTER TABLE `addresses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user` (`fk_user`);
+
+--
+-- Indices de la tabla `carts`
+--
+ALTER TABLE `carts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_product` (`fk_product`),
+  ADD KEY `fk_user` (`fk_user`);
+
+--
+-- Indices de la tabla `ingredients`
+--
+ALTER TABLE `ingredients`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_product` (`fk_product`);
+
+--
+-- Indices de la tabla `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user` (`fk_user`),
+  ADD KEY `fk_payMethod` (`fk_payMethod`);
+
+--
+-- Indices de la tabla `orders_details`
+--
+ALTER TABLE `orders_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_product` (`fk_product`),
+  ADD KEY `fk_order` (`fk_order`);
+
+--
+-- Indices de la tabla `pay_methods`
+--
+ALTER TABLE `pay_methods`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user` (`fk_user`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `addresses`
+--
+ALTER TABLE `addresses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `carts`
+--
+ALTER TABLE `carts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `ingredients`
+--
+ALTER TABLE `ingredients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `orders_details`
+--
+ALTER TABLE `orders_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pay_methods`
+--
+ALTER TABLE `pay_methods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `addresses`
+--
+ALTER TABLE `addresses`
+  ADD CONSTRAINT `addresses_ibfk_1` FOREIGN KEY (`fk_user`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `carts`
+--
+ALTER TABLE `carts`
+  ADD CONSTRAINT `carts_ibfk_1` FOREIGN KEY (`fk_product`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `carts_ibfk_2` FOREIGN KEY (`fk_user`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `ingredients`
+--
+ALTER TABLE `ingredients`
+  ADD CONSTRAINT `ingredients_ibfk_1` FOREIGN KEY (`fk_product`) REFERENCES `products` (`id`);
+
+--
+-- Filtros para la tabla `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`fk_user`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`fk_payMethod`) REFERENCES `pay_methods` (`id`);
+
+--
+-- Filtros para la tabla `orders_details`
+--
+ALTER TABLE `orders_details`
+  ADD CONSTRAINT `orders_details_ibfk_1` FOREIGN KEY (`fk_product`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `orders_details_ibfk_2` FOREIGN KEY (`fk_order`) REFERENCES `orders` (`id`);
+
+--
+-- Filtros para la tabla `pay_methods`
+--
+ALTER TABLE `pay_methods`
+  ADD CONSTRAINT `pay_methods_ibfk_1` FOREIGN KEY (`fk_user`) REFERENCES `users` (`id`);
+COMMIT;
